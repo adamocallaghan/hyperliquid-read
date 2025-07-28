99c6bd94d4e0fdca0e6fc96e1c58864de2424415
Adam O'Callaghan <adam.ocallaghan@yahoo.ie>
2025-07-28 22:27:28 +0100

    deployment script added

---
+++ script/DeployL1Read.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {Script, console2} from "lib/forge-std/src/Script.sol";
+import {L1Read} from "../src/L1Read.sol";
+
+contract DeployL1Read is Script {
+    function run() external {
+
+        // deployer
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        
+        vm.createSelectFork("hyperevm");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // deploy OFT
+        L1Read l1read = new L1Read{salt: "letsread"}();
+        console2.log("L1Read contract deployed on HyperEVM to: ", address(l1read));
+
+        vm.stopBroadcast();
+    }
+}
